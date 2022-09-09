bae70ace95822dc8aa04522842544dcc7c726635
Efstathios Chouliaris <efstathios.chouliaris@thomsonreuters.com>
2022-09-09 16:23:21 -0500

    python test updates

--- python/Person.py
+++ python/Person.py
@@ -1,41 +1,41 @@
 class Person:
-    usernames = []
+    users = []
 
-    def __init__(self, username):
-        if len(username) > 0:
+    def __init__(self, username=None):
+        if username is not None and len(username) > 0:
             self.set_name(username)
 
     def get_usernames(self):
         users_dict = {}
-        for i, username in enumerate(self.usernames):
-            users_dict[i] = username
+        for i, user in enumerate(self.users):
+            users_dict[i] = user
 
         return users_dict
 
     def set_name(self, user_name):
-        self.usernames.append(user_name)
-        return len(self.usernames) - 1
+        self.users.append({'username': user_name})
+        return len(self.users) - 1
 
     def get_id_by_name(self, username):
         users_dict = self.get_usernames()
 
-        if username not in users_dict.values():
-            return 'There is no such user'
-        else:
-            for k in users_dict:
-                if users_dict[k] == username:
-                    return k
+        for k in users_dict:
+            if users_dict[k]['username'] == username:
+                return k
 
-    def get_name_by_id(self, user_id):
+        return {'error': 'There is no such user'}
+
+    def get_user_by_id(self, user_id):
         users_dict = self.get_usernames()
 
         if user_id not in users_dict.keys():
-            return 'There is no such user'
+            return {'error': 'There is no such user'}
         else:
             return users_dict[user_id]
 
 
 if __name__ == '__main__':
     person = Person()
-    print('User Abbas has been added with id ', person.set_name('Abbas'))
-    print('User associated with id 0 is ', person.get_name(0))
+    id = person.set_name('Ευστάθιος')
+    print(f'User Ευστάθιος has been added with id {id}')
+    print(f'User associated with id {id} is ', person.get_user_by_id(id))
--- python/Person_test.py
+++ python/Person_test.py
@@ -5,14 +5,32 @@ import Person
 class Testing(unittest.TestCase):
     expected = 'test'
 
-    def test_set(self):
+    def test_set_success(self):
         person = Person.Person(self.expected)
 
         id = person.get_id_by_name(self.expected)
 
-        name = person.get_name_by_id(id)
+        if type(id) is dict:
+            self.failIf('error' in id,
+                        f"Got error finding user: {id['error']}")
 
-        self.assertEqual(self.expected, name)
+        user = person.get_user_by_id(id)
+
+        if 'error' in user:
+            self.failIf('error' in user,
+                        f"Got error finding user: {user['error']}")
+
+        self.assertEqual(self.expected, user['username'])
+
+    def test_get_fail(self):
+        person = Person.Person()
+
+        id = person.get_id_by_name('wrong')
+
+        user = person.get_user_by_id('wrong')
+
+        self.failIf('error' not in user or type(
+            id) is not dict or 'error' not in id, 'expected error but got none')
 
 
 if __name__ == '__main__':
